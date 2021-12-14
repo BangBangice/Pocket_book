48421d3b8fae3be7c73bb3507296a03d9ac2da9c
lee <343750282@qq.com>
2021-12-14 14:41:38 +0800

    Initial commit

---
+++ app/src/main/java/com/jnu/pocket_book/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.jnu.pocket_book.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
