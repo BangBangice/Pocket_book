3a61fbacfab077d7450acdf415a84ecfab527e19
lee <343750282@qq.com>
2021-12-30 00:57:34 +0800

    修改了更多按钮的响应事件

--- app/src/main/java/com/jnu/pocket_book/PocketBookApplication.java
+++ app/src/main/java/com/jnu/pocket_book/PocketBookApplication.java
@@ -5,7 +5,7 @@ import android.app.Application;
 import com.jnu.pocket_book.db.DBManager;
 
 /* 表示全局应用的类*/
-public class UniteApp extends Application {
+public class PocketBookApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
@@ -13,3 +13,4 @@ public class UniteApp extends Application {
         DBManager.initDB(getApplicationContext());
     }
 }
+
--- app/src/main/java/com/jnu/pocket_book/utils/MoreDialog.java
+++ app/src/main/java/com/jnu/pocket_book/utils/MoreDialog.java
@@ -14,10 +14,9 @@ import androidx.annotation.NonNull;
 import com.jnu.pocket_book.HistoryActivity;
 import com.jnu.pocket_book.MonthChartActivity;
 import com.jnu.pocket_book.R;
-import com.jnu.pocket_book.SettingActivity;
 
 public class MoreDialog extends Dialog implements View.OnClickListener {
-    Button aboutBtn,settingBtn,historyBtn,infoBtn;
+    Button historyBtn,infoBtn;
     ImageView errorIv;
     public MoreDialog(@NonNull Context context) {
         super(context);
@@ -26,25 +25,18 @@ public class MoreDialog extends Dialog implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_more);
-        settingBtn = findViewById(R.id.dialog_more_btn_setting);
         historyBtn = findViewById(R.id.dialog_more_btn_record);
         infoBtn = findViewById(R.id.dialog_more_btn_info);
-        errorIv = findViewById(R.id.dialog_more_iv);
 
-        settingBtn.setOnClickListener(this);
         historyBtn.setOnClickListener(this);
         infoBtn.setOnClickListener(this);
-        errorIv.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         Intent intent = new Intent();
         switch (v.getId()) {
-            case R.id.dialog_more_btn_setting:
-                intent.setClass(getContext(), SettingActivity.class);
-                getContext().startActivity(intent);
-                break;
             case R.id.dialog_more_btn_record:
                 intent.setClass(getContext(), HistoryActivity.class);
                 getContext().startActivity(intent);
@@ -52,8 +44,6 @@ public class MoreDialog extends Dialog implements View.OnClickListener {
             case R.id.dialog_more_btn_info:
                 intent.setClass(getContext(), MonthChartActivity.class);
                 getContext().startActivity(intent);
-                break;
-            case R.id.dialog_more_iv:
                 break;
         }
         cancel();
