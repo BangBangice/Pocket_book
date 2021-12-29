496ada447d8cc8a32a834c3d794d87bc7b63dcf8
lee <343750282@qq.com>
2021-12-30 00:17:50 +0800

    加入数据库

--- README.md
+++
@@ -1,3 +0,0 @@
-# Pocket_book
-an android app to mark your bills, works like a small pocket book. This is a course final test project.
-12.14 14：42 用android studio自带的模板，生成初始程序
--- app/src/main/java/com/jnu/pocket_book/ui/record/OutcomeViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.jnu.pocket_book.ui.record
-
-import androidx.lifecycle.ViewModel
-
-class OutcomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
